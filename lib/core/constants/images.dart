fdd7645bc75c186b14f93a788aeb8613cfa04038
Mohamed S. Bayoumy <mohamedsayedbayoumy639@gmail.com>
2026-07-31 04:53:09 +0300

    feat: Add box truck image asset to AppImages constants

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -3,4 +3,5 @@ abstract class AppImages {
   static String hand = "assets/images/hand.png";
   static String location = "assets/images/location.png";
   static String mapMarker = "assets/images/map-marker.png";
+  static String truck = "assets/images/box-truck.png";
 }
