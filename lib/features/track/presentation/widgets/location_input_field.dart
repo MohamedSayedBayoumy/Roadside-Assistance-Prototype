2659365d76ef5e87821a3a32e9986f6f570541e5
Mohamed S. Bayoumy <mohamedsayedbayoumy639@gmail.com>
2026-07-31 10:56:50 +0300

    feat: Replace FadeIn with ZoomIn animation for location icon in CustomLocationInputField

--- lib/features/track/presentation/widgets/location_input_field.dart
+++ lib/features/track/presentation/widgets/location_input_field.dart
@@ -39,58 +39,60 @@ class CustomLocationInputField extends StatelessWidget {
         border: Border.all(color: AppColors.mainColor),
         borderRadius: BorderRadius.circular(12.0),
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(icon, width: 24),
-          const SizedBox(width: 16.0),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  hintText,
-                  style: const TextStyle(color: Colors.grey, fontSize: 14.0),
-                ),
-                IgnorePointer(
-                  ignoring: true,
-                  child: TextFormField(
-                    controller: controller,
-                    style: AppFonts.style15,
-                    cursorColor: AppColors.mainColor,
-                    decoration: const InputDecoration(
-                      isDense: true,
-                      contentPadding: EdgeInsetsDirectional.only(
-                        top: 4.0,
-                        bottom: 0,
-                        end: 10,
+      child: InkWell(
+        onTap: () async {
+          if (showPickIcon == true) {
+            await trackingScrollController.saveCurrentMapState();
+            final address = await Get.toNamed(AppPaths.pickLocation);
+            callBackWithAddress!(address!);
+          }
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(icon, width: 24),
+            const SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    hintText,
+                    style: const TextStyle(color: Colors.grey, fontSize: 14.0),
+                  ),
+                  IgnorePointer(
+                    ignoring: true,
+                    child: TextFormField(
+                      controller: controller,
+                      style: AppFonts.style15,
+                      cursorColor: AppColors.mainColor,
+                      decoration: const InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsetsDirectional.only(
+                          top: 4.0,
+                          bottom: 0,
+                          end: 10,
+                        ),
+                        border: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
                       ),
-                      border: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          if (showPickIcon == true) ...[
-            const SizedBox(width: 8),
-            InkWell(
-              onTap: () async {
-                await trackingScrollController.saveCurrentMapState();
-                final address = await Get.toNamed(AppPaths.pickLocation);
-                callBackWithAddress!(address!);
-              },
-              child: FadeIn(
+            if (showPickIcon == true) ...[
+              const SizedBox(width: 8),
+              ZoomIn(
                 duration: Duration(seconds: 1),
                 child: Image.asset(AppImages.location, width: 30),
               ),
-            ),
+            ],
           ],
-        ],
+        ),
       ),
     );
   }
