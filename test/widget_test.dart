435315cb8b01413dc27b0bb1681b01ac552f81d0
Mohamed S. Bayoumy <mohamedsayedbayoumy639@gmail.com>
2026-08-01 05:58:57 +0300

    feat: Add mocktail dependency and implement LoginUseCase validation tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,57 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:freelance/core/error/common_failed_model.dart';
+import 'package:freelance/features/login/domain/login_entity/login_entity_body.dart';
+import 'package:freelance/features/login/domain/login_repo/login_repo.dart';
+import 'package:freelance/features/login/domain/login_use_case/login_use_case.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'package:freelance/main.dart';
+class MockLoginRepo extends Mock implements LoginRepo {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  late LoginUseCase loginUseCase;
+  late MockLoginRepo mockLoginRepo;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  setUp(() {
+    mockLoginRepo = MockLoginRepo();
+    loginUseCase = LoginUseCase(loginRepo: mockLoginRepo);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  group('LoginUseCase Validation Tests', () {
+    test(
+      'should return Left(DioFailure) when email and password are both invalid',
+      () async {
+        // Arrange
+        final invalidBody = LoginEntityBody(
+          user: 'invalid_email_format',
+          password: '123',
+        );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+        final result = await loginUseCase.login(body: invalidBody);
+
+        expect(result.isLeft(), true);
+
+        result.fold((failure) {
+          expect(failure, isA<DioFailure>());
+
+          // expect((failure as DioFailure).failureMessage, isNotEmpty);
+        }, (_) => fail('Should not return Right when inputs are invalid'));
+      },
+    );
+
+    test('should return Right(true) when inputs are valid', () async {
+      final validBody = LoginEntityBody(
+        user: 'test@example.com',
+        password: 'StrongPassword123!',
+      );
+      final result = await loginUseCase.login(body: validBody);
+
+      expect(result.isRight(), true);
+
+      result.fold((_) => fail('Should not return Left when inputs are valid'), (
+        success,
+      ) {
+        expect(success, true);
+      });
+    });
   });
 }
